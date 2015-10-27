eb7e7be3eaf9e728895fe3fff40a2d8afcf14562
phamtuanhoang <phamtuanhoang_1612@yahoo.com>
2015-10-27 15:14:23 +0800

    Initial Commit

---
+++ daily/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  daily
+//
+//  Created by tuanhoang pham on 27/10/15.
+//  Copyright © 2015 tuanhoang pham. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ dailyTests/dailyTests.swift
@@ -0,0 +1,36 @@
+//
+//  dailyTests.swift
+//  dailyTests
+//
+//  Created by tuanhoang pham on 27/10/15.
+//  Copyright © 2015 tuanhoang pham. All rights reserved.
+//
+
+import XCTest
+@testable import daily
+
+class dailyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
